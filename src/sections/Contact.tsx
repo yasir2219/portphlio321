04af6d2d1491be46c2ad6cca52cb2a360e9b6cfd
Yasir arafat <yasirarafat2606@gmail.com>
2025-01-30 02:52:47 +0600

    PortPholio321

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -9,19 +9,23 @@ export const ContactSection = () => {
           <div
             className="absolute inset-0 opacity-5 -z-10"
             style={{ backgroundImage: `url(${grainImage.src})` }}
+            aria-hidden="true"
           ></div>
           <div className="flex flex-col md:flex-row items-center gap-8 md:gap-16">
             <div>
               <h2 className="font-serif text-2xl md:text-3xl">
-                Let's create something amazing together
+                Let&apos;s create something amazing together
               </h2>
-              <p className="text-sm md:text-base mt-2 ">
-                Ready to bring your next project to life? let's connect and
-                dicuss how I can help you achieve your goals.
+              <p className="text-sm md:text-base mt-2">
+                Ready to bring your next project to life? Let&apos;s connect and
+                discuss how I can help you achieve your goals.
               </p>
             </div>
             <div>
-              <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900">
+              <button
+                className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900 hover:bg-gray-800 transition"
+                aria-label="Contact Me"
+              >
                 <span className="font-semibold">Contact Me</span>
                 <span>
                   <ArrowUpRightIcon className="size-4" />
